e4476d0673cb5d4f81022f860551960c07713b11
Obasan Tolulope <obasantolu@gmail.com>
2025-08-25 13:51:13 +0100

    feat: created the application page with the dashboard and other sideBar items

---
+++ app/app/create/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateEventsPage = () => {
+  return (
+    <div>CreateEventsPage</div>
+  )
+}
+
+export default CreateEventsPage
---
+++ app/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import SideBar from "../components/app/SideBar";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} flex min-h-screen`}>
+        {/* Sidebar */}
+        <SideBar />
+
+        {/* Main Content */}
+        <main className="flex-1 p-6 bg-gray-50">{children}</main>
+      </body>
+    </html>
+  );
+}
