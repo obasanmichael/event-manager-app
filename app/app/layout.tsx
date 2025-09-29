6081dff7b27c48861bf014d0502f1909e95494c1
Obasan Tolulope <obasantolu@gmail.com>
2025-09-29 16:21:49 +0100

    switched client to use createBrowserClient for proper cookie handling

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import SideBar from "../components/app/SideBar";
 import "../globals.css";
+import AuthListener from "../components/AuthListener";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export default function DashboardLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} flex min-h-screen`}>
+        <AuthListener />
         <SideBar />
         <main className="flex-1 pt-10 bg-gray-50">{children}</main>
         
